ccc781cb80efab3eb154b28479c3392f62003427
melattgirmay <melatgirmay20@gmail.com>
2023-06-28 06:46:03 -0700

    Add file

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,10 +3,10 @@
 const fs = require('fs');
 const [sourceFile1, sourceFile2, destinationFile] = process.argv.slice(2);
 
-const content1 = fs.readFileSync(sourceFile1, 'utf8');
-const content2 = fs.readFileSync(sourceFile2, 'utf8');
-const concatenatedContent = content1 + content2;
+const content1 = fs.readFileSync(sourceFile1, 'utf8').trim();
+const content2 = fs.readFileSync(sourceFile2, 'utf8').trim();
+const concatenatedContent = `${content1}\n${content2}\n`;
 
 fs.writeFileSync(destinationFile, concatenatedContent);
 
-console.log(concatenatedContent);
+console.log(concatenatedContent.trim());
