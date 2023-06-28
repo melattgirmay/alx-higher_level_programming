54910b938fda92ce6ac57cfcf2cec85b53423d52
melattgirmay <melatgirmay20@gmail.com>
2023-06-28 06:36:49 -0700

    Update file

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,12 +3,11 @@
 const { dict } = require('./101-data');
 const result = {};
 for (const userId in dict) {
-	const occurrence = dict[userId];
-	if (result[occurrence]) {
-		result[occurrence].push(userId);
-	}
-	else {
-		result[occurrence] = [userId];
-	}
+  const occurrence = dict[userId];
+  if (result[occurrence]) {
+    result[occurrence].push(userId);
+  } else {
+    result[occurrence] = [userId];
+  }
 }
 console.log(result);
--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,17 +3,11 @@
 const fs = require('fs');
 const [sourceFile1, sourceFile2, destinationFile] = process.argv.slice(2);
 
-// Read the contents of the first source file
 const content1 = fs.readFileSync(sourceFile1, 'utf8');
-
-// Read the contents of the second source file
 const content2 = fs.readFileSync(sourceFile2, 'utf8');
+const concatenatedContent = content1 + content2;
 
-// Concatenate the contents of both files
-const concatenatedContent = content1 + '\n' + content2;
-
-// Write the concatenated content to the destination file
 fs.writeFileSync(destinationFile, concatenatedContent);
 
-console.log(`Concatenation successful. Result written to ${destinationFile}.`);
+console.log(concatenatedContent);
 
