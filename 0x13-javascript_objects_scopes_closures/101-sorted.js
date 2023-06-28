7075b3fcc747dd58a6e1cdb934d865dca0cbea6f
melattgirmay <melatgirmay20@gmail.com>
2023-06-28 06:07:50 -0700

    Update file

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,21 +1,13 @@
 #!/usr/bin/node
 
 const { dict } = require('./101-data');
-
 const result = {};
-
-// Loop through the original dictionary
 for (const userId in dict) {
-  const occurrence = dict[userId];
-  
-  // Check if the occurrence exists as a key in the result dictionary
-  if (result[occurrence]) {
-    // If the occurrence exists, add the current userId to the list
-    result[occurrence].push(userId);
-  } else {
-    // If the occurrence doesn't exist, create a new key and set the value as an array with the current userId
-    result[occurrence] = [userId];
+	const occurrence = dict[userId];
+	if (result[occurrence]) {
+		result[occurrence].push(userId);
+	} else {
+		result[occurrence] = [userId];
   }
 }
-
 console.log(result);
