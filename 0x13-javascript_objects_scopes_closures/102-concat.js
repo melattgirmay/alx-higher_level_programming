116e33cbc962ec60be24f5212f5b85c5b9bf588a
melattgirmay <melatgirmay20@gmail.com>
2023-07-21 05:12:40 -0700

    Update file

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,17 @@
 #!/usr/bin/node
 const fs = require('fs');
-const src1 = fs.readFileSync(process.argv[2], 'utf8');
-const src2 = fs.readFileSync(process.argv[3], 'utf8');
-fs.writeFileSync(process.argv[4], src1 + src2);
+
+const src1Path = process.argv[2];
+const src2Path = process.argv[3];
+const destPath = process.argv[4];
+
+const src1Content = fs.readFileSync(src1Path, 'utf8');
+const src2Content = fs.readFileSync(src2Path, 'utf8');
+
+const concatContent = src1Content + src2Content;
+
+fs.writeFileSync(destPath, concatContent);
+
+// Print the output to the console for verification
+console.log(concatContent);
+
